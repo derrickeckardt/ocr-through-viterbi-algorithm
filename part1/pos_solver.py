f7b6f3bcf0955b8f62a61ec359876ace1e36f1ea
derrick eckardt <derrick.eckardt@gmail.com>
2018-11-10 21:38:12 +0000

    Simple model done

--- part1/pos_solver.py
+++ part1/pos_solver.py
@@ -14,6 +14,7 @@
 import random
 import math
 from collections import defaultdict, Counter
+from operator import itemgetter
 
 
 # We've set up a suggested code structure, but feel free to change it. Just
@@ -26,20 +27,26 @@ class Solver:
     #  with a given part-of-speech labeling. Right now just returns -999 -- fix this!
     def __init__(self):
         self.p_s1 = Counter()
+        self.p_si = Counter()
+        self.p_wi = Counter()
+        self.unique_words = 0.0
         self.p_si1_si = {} # Previous state
         self.p_si2_si = {} # Two states agp
         self.p_wi_si = {}
-        for pos in ["adj","adv","adp","conj","det","noun","num","pron","prt","verb","x","."]:
+        self.pos = ["adj","adv","adp","conj","det","noun","num","pron","prt","verb","x","."]
+        for pos in self.pos:
              self.p_wi_si[pos] = Counter()
              self.p_si1_si[pos] = Counter()
              self.p_si2_si[pos] = Counter()
     
     def posterior(self, model, sentence, label):
         if model == "Simple":
-            answer = 0
-            # for word in sentence:
-            #     answer += log(self.p_wi_si)
-            return answer    
+            interim = 0
+            for word,part in zip(sentence,label):
+# Need to devide by denominator
+                word_interim = (self.p_wi_si[part][word] / float(self.p_si[part])) * ((self.p_si[part])/float(self.unique_words))
+                interim += math.log(word_interim) if word_interim != 0 else math.log(1/float(self.unique_words)) 
+            return interim
         elif model == "Complex":
             return -999
         elif model == "HMM":
@@ -57,6 +64,8 @@ class Solver:
             last_pos = ""
             two_ago_pos =""
             for word, part in zip(each[0],each[1]):
+                self.p_si[part] += 1
+                self.p_wi[word] += 1
                 if word in self.p_wi_si[part]:
                     self.p_wi_si[part][word] += 1
                 else:
@@ -74,19 +83,29 @@ class Solver:
                     two_ago_pos = last_pos 
                 last_pos = part
 
-        print self.p_s1
-        print self.p_si1_si
-        print self.p_si2_si
+        # Totals for use later
+        self.unique_words = sum(self.p_si.values())
+        # print self.p_s1
+        # print self.p_si1_si
+        # print self.p_si2_si
         # return self.p_s1, self.p_si1_si, self.p_wi_si
     
     # Functions for each algorithm. Right now this just returns nouns -- fix this!
     #
     def simplified(self, sentence):
+        print sentence
         simplified_model = []
-        for item in sentence:
-            simplified_model.extend([self.p_s1[item]])
-        print simplified_model
-        return [ "noun" ] * len(sentence)
+        for word in sentence:
+            pos_values = []
+            for pos in self.pos:
+# Need to divide by denominator
+                interim = (self.p_wi_si[pos][word] / float(self.p_si[pos])) * ((self.p_si[pos])/float(self.unique_words))
+                pos_values.extend([[interim, pos]])
+            pos_values = sorted(pos_values, key=itemgetter(0), reverse=True)
+            # Note uses most popular case in case word is not in training corpus
+            most_likely = pos_values[0][1] if pos_values[0][0] != 0 else self.p_si.most_common(1)[0][0]
+            simplified_model.extend([most_likely])
+        return simplified_model 
 
     def complex_mcmc(self, sentence):
         return [ "noun" ] * len(sentence)
