24817afe7fd89389155706b007d0a9db1e3a18b6
derrick eckardt <derrick.eckardt@gmail.com>
2018-11-12 02:31:16 +0000

    working on viterbi

--- part1/pos_solver.py
+++ part1/pos_solver.py
@@ -15,6 +15,8 @@ import random
 import math
 from collections import defaultdict, Counter
 from operator import itemgetter
+from pprint import pprint
+from time import sleep
 
 
 # We've set up a suggested code structure, but feel free to change it. Just
@@ -29,7 +31,8 @@ class Solver:
         self.p_s1 = Counter()
         self.p_si = Counter()
         self.p_wi = Counter()
-        self.unique_words = 0.0
+        self.unique_words = 0
+        self.unique_lines = 0
         self.p_si1_si = {} # Previous state
         self.p_si2_si = {} # Two states agp
         self.p_wi_si = {}
@@ -45,6 +48,7 @@ class Solver:
             for word,part in zip(sentence,label):
 # Need to devide by denominator
                 word_interim = (self.p_wi_si[part][word] / float(self.p_si[part])) * ((self.p_si[part])/float(self.unique_words))
+# Double check the use of math.log(1/float(self,unique words)
                 interim += math.log(word_interim) if word_interim != 0 else math.log(1/float(self.unique_words)) 
             return interim
         elif model == "Complex":
@@ -85,6 +89,7 @@ class Solver:
 
         # Totals for use later
         self.unique_words = sum(self.p_si.values())
+        self.unique_lines = sum(self.p_s1.values())
         # print self.p_s1
         # print self.p_si1_si
         # print self.p_si2_si
@@ -102,7 +107,7 @@ class Solver:
                 interim = (self.p_wi_si[pos][word] / float(self.p_si[pos])) * ((self.p_si[pos])/float(self.unique_words))
                 pos_values.extend([[interim, pos]])
             pos_values = sorted(pos_values, key=itemgetter(0), reverse=True)
-            # Note uses most popular case in case word is not in training corpus
+            # Note uses most popular pos in case word is not in training corpus
             most_likely = pos_values[0][1] if pos_values[0][0] != 0 else self.p_si.most_common(1)[0][0]
             simplified_model.extend([most_likely])
         return simplified_model 
@@ -111,6 +116,45 @@ class Solver:
         return [ "noun" ] * len(sentence)
 
     def hmm_viterbi(self, sentence):
+        viterbi_model = []
+        # first word in sentence
+        for pos in self.pos:
+            # sublist to the form of [position_no, p(si), came_from_pos, pos], only with the words that appear
+            if self.p_wi_si[pos][sentence[0]] != 0:
+                viterbi_model.extend([[0,self.p_s1[pos]/float(self.unique_lines)*self.p_wi_si[pos][sentence[0]]/float(self.p_si[pos]), "self", pos]])
+        # Double check in case there are no values, will generate with each pos as if there was exactly one occurance
+        if len(viterbi_model) == 0:
+            for pos in self.pos:
+                viterbi_model.extend([[0,self.p_s1[pos]/float(self.unique_lines)*1/self.p_si[pos], "self", pos]])
+            
+        # second word to last word in sentence
+        viterbi_temp = viterbi_model*1
+        for word, i in zip(sentence[1:],range(1,len(sentence))):
+            viterbi_last = viterbi_temp * 1
+            viterbi_temp = []
+            for n, value, predecessor, old_pos in viterbi_last:
+                for pos in self.pos:
+                    if self.p_wi_si[pos][word] !=0 and self.p_si1_si[old_pos][pos] !=0:
+                        #print n, value, predecessor, old_pos
+                        #sleep(5)
+                        viterbi_temp.extend([[i,self.p_si1_si[old_pos][pos]/float(self.p_si[old_pos])*self.p_wi_si[pos][word]/float(self.p_si[pos]), old_pos, pos]])
+                if len(viterbi_temp) == 0:
+                    if self.p_si1_si[pos][word] !=0:
+                        viterbi_temp.extend([[i,1/float(self.p_si[old_pos])*self.p_wi_si[pos][word]/float(self.p_si[pos]), old_pos, pos]])
+                    else:
+                        viterbi_temp.extend([[i,1/float(self.p_si[old_pos])*1/float(self.p_si[pos]), old_pos, pos]])
+                        
+            viterbi_model.extend(viterbi_temp)
+            print viterbi_temp
+            sleep(5)
+        
+        
+        # backtrack now
+        if len(viterbi_model) > 40:
+            print viterbi_model[-40:]
+        else:
+            print viterbi_model
+            
         return [ "noun" ] * len(sentence)
 
 
